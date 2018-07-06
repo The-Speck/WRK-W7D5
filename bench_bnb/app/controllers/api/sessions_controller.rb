306e37e03baca342d404cf46f8c994e2c27cc099
Alex <a.plyushchay@gmail.com>
2018-07-06 15:29:16 -0400

    Complete Phase 1

--- bench_bnb/app/controllers/api/sessions_controller.rb
+++ bench_bnb/app/controllers/api/sessions_controller.rb
@@ -1,16 +1,26 @@
-class SessionController < ApplicationController
+class Api::SessionsController < ApplicationController
 
   def create
-    username = user_params(:username)
-    password = user_params(:password)
+    username = user_params[:username]
+    password = user_params[:password]
 
     @user = User.find_by_credentials(username, password)
+    if @user
+      log_in(@user)
+      render :show
+    else
+      render json: ['Invalid Username/Password'], status: 422
+    end
 
-    
   end
 
   def destroy
-
+    if current_user
+      log_out
+      render json: {}
+    else
+      render json: {}, status: 404
+    end
   end
 
   private
--- bench_bnb/frontend/bench_bnb.jsx
+++ bench_bnb/frontend/bench_bnb.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+// Test
+import * as APIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
+
+window.signup = APIUtil.signup;
+window.login = APIUtil.login;
